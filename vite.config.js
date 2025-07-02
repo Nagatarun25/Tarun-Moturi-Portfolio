3ecbcf6bb650f193198b897c7e8585af09832b0b
Nagatarun25 <nagatarunmoturi@gmail.com>
2025-07-02 15:52:19 -0400

    Add GitHub Actions deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Tarun-Moturi-Portfolio/', // Must match your repo name
   plugins: [react()],
-  base: "/Tarun-Moturi-Portfolio/",
-})
+});
