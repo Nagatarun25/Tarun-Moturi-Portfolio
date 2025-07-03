edd06f9d656a42f5758b7723bc3643db87bff811
Tarun Moturi <69943945+Nagatarun25@users.noreply.github.com>
2025-07-03 01:31:03 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Must match your repo name
+  base: '/Tarun-Moturi-Portfolio/', // Must match your repo name
   plugins: [react()],
 });
